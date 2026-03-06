73287602a26472baa0b654f7a1c94a50b4f197c3
xDarKSchneideR <102815217+xDarKSchneideR@users.noreply.github.com>
2026-03-06 19:53:18 +0100

    Inicial
    Primera subida

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'garantia-box-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(['./', './index.html']);
+    })
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
